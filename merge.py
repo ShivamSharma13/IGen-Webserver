9202992d263baca9006b04342217cbe691ed1d2b
dkundnani3 <dkundnani3@gatech.edu>
2020-12-04 00:09:30 -0500

    merging optimized for larger file before imputation

--- merge.py
+++ merge.py
@@ -14,14 +14,24 @@ parser.add_argument('-o','--out', required=True, help='Output folder/path')
 parser.add_argument('-c','--col', required=True, help='Column name for Genotype information to be extracted from subject vcf file. ')
 args = parser.parse_args()
 
+
 print ("Reference File exists:"+str(os.path.exists(args.reference)))
 print ("Subject File exists:" + str(os.path.exists(args.test)))
 
+#Create output directory if it doesn't already exists
+if not os.path.exists(args.out):
+    os.makedirs(args.out)
+
 if os.path.exists(args.reference) and os.path.exists(args.test):
+    #Making the reference file short by getting rsids present in the sample vcf
+    subprocess.call("cat "+args.test+" | cut -f3 | grep 'rs' > rsids.txt", shell=True, universal_newlines=True)
+    subprocess.call("grep '#CHR' "+args.reference+">./"+args.out+"/reference.vcf", shell=True, universal_newlines=True)
+    awk="awk 'BEGIN{while((getline<'rsids.txt')>0)l[$1]=1}/^#/||l[$3]' "
+    subprocess.call("bash getrsids.sh "+args.reference+" "+args.out+"/reference.vcf", shell=True, universal_newlines=True)#The script location needs to be updated!!!!!!!!!!!!!1
 
     #Removing comments from vcf files
-    subprocess.call("grep -v '##' "+args.reference+" > reference.vcf", shell=True, universal_newlines=True)
-    subprocess.call("grep -v '##' "+args.test+" > sample.vcf", shell=True, universal_newlines=True)
+    #subprocess.call("grep -v '##' "+args.reference+" > reference.vcf", shell=True, universal_newlines=True)
+    subprocess.call("grep -v '##' "+args.test+" > "+args.out+"/sample.vcf", shell=True, universal_newlines=True)
     
     #Taking in both sets of vcf files
     df=pd.read_csv('reference.vcf', sep='\t')
@@ -30,9 +40,7 @@ if os.path.exists(args.reference) and os.path.exists(args.test):
     #Merging only the genotype col from subject vcf file with the reference vcf based on rsids
     merge=df.merge(sample[['ID', args.col]], on="ID", how="inner")
     
-    #Create output directory if it doesn't already exists
-    if not os.path.exists(args.out):
-        os.makedirs(args.out)
+    
     #Saving merged.vcf file in the output directory
     merge.to_csv(args.out+'/'+'merged.vcf', sep='\t', index=False)
     print("Merged file saved in "+args.out+" directory as merged.vcf")
