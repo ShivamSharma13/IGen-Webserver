03693bbe51543b5c2a3382095543f5c9bc53f21b
Shivam Sharma <mailtoshivam13@gmail.com>
2020-12-06 15:40:30 -0500

    deleted redundant files

--- PCA.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#first argument is unique output location 
-
-bgzip merged.vcf 
-tabix merged.vcf.gz
-
-plink --vcf merged.vcf.gz --make-bed --pca --out ${1}/PCA/1000G_PCA_merged
--- getrsids.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-
-awk 'BEGIN{while((getline<"rsids.txt")>0)l[$1]=1}/^#/||l[$3]' $1 > $2
