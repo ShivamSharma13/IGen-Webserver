c605c5fc064bf8b0e21738b6118fdec436937170
Shivam Sharma <mailtoshivam13@gmail.com>
2020-12-06 20:27:58 -0500

    Onto PCA, fixed merge script sample.vcf error 3

--- IGenWebServer/pipeline-scripts/PCA.sh
+++ IGenWebServer/pipeline-scripts/PCA.sh
@@ -2,7 +2,7 @@
 
 #first argument is unique output location 
 
-bgzip ${1}/merged.vcf 
-tabix ${1}/merged.vcf.gz
+bgzip ${1}/PCA_merge/merged.vcf 
+tabix ${1}/PCA_merge/merged.vcf.gz
 
-plink --vcf ${1}/merged.vcf.gz --make-bed --pca --out ${1}/PCA/1000G_PCA_merged
+plink --vcf ${1}/PCA_merge/merged.vcf.gz --make-bed --pca --out ${1}/PCA/1000G_PCA_merged
