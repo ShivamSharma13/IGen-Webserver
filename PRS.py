d9b3674a29eed043f431c7e872ab21c61a5f9178
dkundnani3 <dkundnani3@gatech.edu>
2020-12-06 22:46:36 -0500

    final PRS

--- PRS.py
+++ PRS.py
@@ -16,15 +16,16 @@ parser.add_argument('-o','--out', required=True, help='Output folder/path(full p
 parser.add_argument('-r','--results', required=True, help='Results folder/path(full path)')
 parser.add_argument('-c','--col', required=True, help='Column name for Genotype information to be extracted from subject vcf file. ')
 args = parser.parse_args()
+'''
+parser = argparse.ArgumentParser(description='example: python3 PRS.py -s ~/scratch/stats/Infections -m ~/scratch/merge/mergedforPRS.vcf -o ~/scratch/stats/PRS -r ~/scratch/stats/percentile -c INFO_y')
+parser.add_argument('-s','--SNP', default='/storage/home/hcoda1/5/dkundnani3/scratch/stats/Infections' ,help='full path to folder with summary statitics')
+parser.add_argument('-m','--merged', default='/storage/home/hcoda1/5/dkundnani3/scratch/merge/mergedforPRS.vcf' , help='full path of merged vcf files after imputation')
+parser.add_argument('-o','--out', default='PRS', help='Output folder/path(full path)')
+parser.add_argument('-r','--results', default='percentile', help='Results folder/path(full path)')
+parser.add_argument('-c','--col', default='INFO_y', help='Column name for Genotype information to be extracted from subject vcf file. ')
+args = parser.parse_args()
+'''
 
-def PRS(statloc,output,filename,basename):
-    subprocess.call("awk '{print $2,$7}' '"+statloc+"/"+filename+"' > "+basename+".snp.pvalue", shell=True, universal_newlines=True)
-    subprocess.call("plink --bfile final --clump-kb 250 --clump '"+statloc+"/"+filename+"' --clump-snp-field SNP --clump-field P --out "+basename+" ", shell=True, universal_newlines=True)
-    if os.path.exists(output+basename+'.clumped'):
-        subprocess.call("awk 'NR!=1{print $3}' "+basename+".clumped > "+basename+".valid.snp", shell=True, universal_newlines=True)
-        subprocess.call("plink --bfile final --score "+statloc+"/"+filename+" 2 5 8 header sum --extract "+basename+".valid.snp -out "+basename+" ", shell=True, universal_newlines=True)
-    else:
-        subprocess.call("plink --bfile final --score "+statloc+"/"+filename+" 2 5 8 header -out "+basename+" ", shell=True, universal_newlines=True)
 
 def percentile(output,basename,name):
     ID=[] #list of IDs of population
@@ -52,6 +53,7 @@ def percentile(output,basename,name):
 #Create output directory if it doesn't already exists
 if not os.path.exists(args.out):
     os.makedirs(args.out)
+
 if not os.path.exists(args.results):
     os.makedirs(args.results)
 
@@ -60,15 +62,16 @@ print ("Merged File exists:" + str(os.path.exists(args.merged)))
 
 
 ###Converting the Vcf file to different plink compatible formats in the output directory
-os.chdir(args.out)
+#os.chdir(args.out)
 
-subprocess.call("vcftools --vcf "+args.merged+" --plink --out final", shell=True, universal_newlines=True)
-subprocess.call("plink --file final --make-bed --out final", shell=True, universal_newlines=True)
-subprocess.call("plink --bfile final --maf 0.01 --mind 0.01 --write-snplist --make-just-fam --out final", shell=True, universal_newlines=True)
+subprocess.call("vcftools --vcf "+args.merged+" --plink --out "+args.out+"/final", shell=True, universal_newlines=True)
+subprocess.call("plink --file "+args.out+"/final --make-bed --out "+args.out+"/final", shell=True, universal_newlines=True)
+subprocess.call("plink --bfile "+args.out+"/final --maf 0.01 --mind 0.01 --write-snplist --make-just-fam --out "+args.out+"/final", shell=True, universal_newlines=True)
 
 #Getting the list of all the infection files existing in the specified folder
 Infections=os.listdir(args.SNP)
 Infections.sort()
+o=open(args.results+"/percentiles.txt", "w")
 
 ###Calculating PRS and getting the percentile for every infection
 if os.path.exists(args.SNP):
@@ -77,38 +80,29 @@ if os.path.exists(args.SNP):
         inf=infect.replace("_", " ")
         
         # Clumping and Calculating PRS score
-        PRS(args.SNP,args.out,infection,infect)
+        subprocess.call("awk '{print $2,$7}' "+args.SNP+"/"+infection+" > "+args.out+"/"+infect+".snp.pvalue", shell=True, universal_newlines=True)
+        subprocess.call("plink --bfile "+args.out+"/final --clump-p1 1 --clump-kb 250 --clump '"+args.SNP+"/"+infection+"' --clump-snp-field SNP --clump-field P --out "+args.out+"/"+infect+" ", shell=True, universal_newlines=True)
+        if os.path.exists(args.out+"/"+infect+'.clumped'):
+            subprocess.call("awk 'NR!=1{print $3}' "+args.out+"/"+infect+".clumped > "+args.out+"/"+infect+".valid.snp", shell=True, universal_newlines=True)
+            subprocess.call("plink --bfile "+args.out+"/final --score "+args.SNP+"/"+infection+" 2 5 8 header --extract "+args.out+"/"+infect+".valid.snp --out "+args.out+"/"+infect+" ", shell=True, universal_newlines=True)
+        else:
+            subprocess.call("plink --bfile "+args.out+"/final --score "+args.SNP+"/"+infection+" 2 5 8 header --out "+args.out+"/"+infect+" ", shell=True, universal_newlines=True)
         
         #Calculating percentile for each infection from the PRS scores file
         perc=percentile(args.out,infect,args.col)
         perc=str(perc)
-        
-        #Create percentiles.txt if it doesn't exist in the results folder
-        if os.path.exists(args.results+'/percentiles.txt'):
-            subprocess.call("rm "+args.results+"/percentiles.txt",shell=True, universal_newlines=True)
-        else:
-            subprocess.call("touch "+args.results+"/percentiles.txt",shell=True, universal_newlines=True)
+        print("percentile:"+perc)
             
         #Append your infection name and percentile risk/score for the individual in the results folder under percentiles.txt
-        subprocess.call("echo "+inf+"\t"+perc+">>"+args.results+"/percentiles.txt", shell=True, universal_newlines=True)
         
+        o.write(inf+"\t"+perc+"\n")
+    
 else:
     print("Please make sure the file names and location are correct")
     
     
+o.close()    
     
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
